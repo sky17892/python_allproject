2ddd2f4f1cf7a0e0a7cc1252f4f293a705d7656d
sky17893 <sky17893@gmail.com>
2026-01-21 10:53:29 +0900

    python commit

---
+++ closure.py
@@ -0,0 +1,37 @@
+def make_counter():
+    count = 0
+
+    def counter():
+        nonlocal count
+        count += 1
+        return count
+    
+    return counter
+    
+counter1 = make_counter()
+
+print(counter1())
+print(counter1())
+print(counter1())
+
+counter2 = make_counter()
+
+print(counter2())
+print(counter2())
+print(counter2())
+print(counter2())
+
+class Counter:
+    def __init__(self):
+        self.count = 0
+    def incmethod(self):
+        self.count += 1
+        return self.count
+    
+counter3 = Counter()
+
+print(counter3.incmethod())
+print(counter3.incmethod())
+print(counter3.incmethod())
+print(counter3.incmethod())
+print(counter3.incmethod())
